2b931b17204cc8d69ed3238d289e90247bca1586
xylafur <james@jkesley.com>
2020-06-27 12:48:47 -0500

    Add ability to change duty cycle

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 #include "stm32f0xx_conf.h"
 
 #include "stm32f0xx_tim.h"
+#include "stm32f0xx_usart.h"
+#include <stdio.h>
+
+#ifdef __GNUC__
+  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
 
 #define ARR_MAX 0xFFFF
 #define PRESCALE_MAX 0xFFFF
+#define CCR_MAX 0xFFFF
 
 typedef uint32_t status_t;
 
@@ -15,23 +27,35 @@ typedef uint32_t status_t;
 #define KHZ(val) (val * 1000)
 #define MHZ(val) (val * 1000000)
 
+status_t pwm_ch_duty(uint32_t duty);
 
 void SysTick_Handler(void) {
     static uint32_t ii = 0;
     static uint16_t tick = 0;
+    static status_t st = STATUS_SUCCESS;
+
+    /* If anything ever went wrong, just stop processing */
+    if (FAIL(st)) {
+        return;
+    }
 
     switch (tick++) {
-        case 400:
+        case 100:
             if (ii == 0) {
-                pwm_ch_freq(MHZ(10), .9);
+                st = pwm_ch_duty(250);
                 ii = 1;
 
             } else {
-                pwm_ch_freq(MHZ(10), .4);
+                st = pwm_ch_duty(127);
                 ii = 0;
             }
             tick = 0;
             break;
+    }
+
+    /* Turn on the LED if anything goes wrong */
+    if (FAIL(st)){
+        GPIOC->ODR = (1 << 8);
     }
 }
 
@@ -49,39 +73,44 @@ void init_pins()
     GPIO_Init(GPIOB, &gpioStructure);
 }
 
-/*  Initializes Timer 3 to have a period of pwm_freq and returns the period
- */
-status_t init_timer(uint32_t pwm_freq, uint32_t* clk_freq, uint32_t* period)
-{
+status_t calc_vals(uint32_t pwm_freq, uint32_t* prescale, uint32_t* period) {
     const uint32_t fclk = SystemCoreClock;
-
-    uint32_t prescale = 0xdeadbeef;
-
-    *period = ARR_MAX + 1;
+    *prescale = 0xdeadbeef;
 
     /* Find prescale value such that the pwm_freq can be acieved */
     /* Using: fpwm = (fclk / ( (ARR + 1) ( PSC + 1)) */
     while (*period > ARR_MAX) {
-        prescale = prescale == 0xdeadbeef ? 0 : prescale + 1;
-        *period = fclk / (pwm_freq / (prescale + 1));
+        *prescale = *prescale == 0xdeadbeef ? 0 : *prescale + 1;
+        *period = fclk / (pwm_freq / (*prescale + 1)) - 1;
     }
 
-    if (prescale > ARR_MAX) {
+    if (*prescale >= PRESCALE_MAX) {
         return STATUS_ERROR;
     }
 
-    *clk_freq = fclk / (prescale + 1);
+    return STATUS_SUCCESS;
+}
 
+/*  Inits Timer 3 to have the particular frequency and period
+ */
+status_t init_timer(uint32_t pwm_freq, uint32_t* period) {
+    status_t st;
+    uint32_t prescale;
+
+    st = calc_vals(pwm_freq, &prescale, period);
+    if (FAIL(st)) {
+        return st;
+    }
 
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
-    TIM_TimeBaseInitTypeDef timerInitStructure;
-
-    timerInitStructure.TIM_Prescaler = prescale;
-    timerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-    timerInitStructure.TIM_Period = *period;
-    timerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-    timerInitStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInitTypeDef timerInitStructure = {
+        .TIM_Prescaler = prescale,
+        .TIM_CounterMode = TIM_CounterMode_Up,
+        .TIM_Period = *period,
+        .TIM_ClockDivision = TIM_CKD_DIV1,
+        .TIM_RepetitionCounter = 0,
+    };
 
     TIM_TimeBaseInit(TIM3, &timerInitStructure);
     TIM_Cmd(TIM3, ENABLE);
@@ -89,72 +118,94 @@ status_t init_timer(uint32_t pwm_freq, uint32_t* clk_freq, uint32_t* period)
     return STATUS_SUCCESS;
 }
 
+uint32_t global_period;
+
 /*  Initializes PWM for the given frequency / duty cycle on timer 3 channel 1
  *
  *  Timer 3 Channel 1 coresponds to PB4
  */
-status_t init_pwm(uint32_t pwm_freq, float duty)
+status_t init_pwm(uint32_t pwm_freq)
 {
     status_t st;
-    uint32_t clk_freq, period;
+    uint32_t period;
+
 
     init_pins();
 
-    st = init_timer(pwm_freq, &clk_freq, &period);
+    st = init_timer(pwm_freq, &period);
     if (FAIL(st)) {
         return st;
     }
 
-    TIM_OCInitTypeDef outputChannelInit = {0,};
+    TIM_OCInitTypeDef channel = {
+        .TIM_OCMode = TIM_OCMode_PWM1,
+        /* Default to a duty of 50% */
+        .TIM_Pulse = period * .5,
+        .TIM_OutputState = TIM_OutputState_Enable,
+        .TIM_OCPolarity = TIM_OCPolarity_High,
+    };
 
-    outputChannelInit.TIM_OCMode = TIM_OCMode_PWM1;
-
-    outputChannelInit.TIM_Pulse = period * duty;
-    outputChannelInit.TIM_OutputState = TIM_OutputState_Enable;
-    outputChannelInit.TIM_OCPolarity = TIM_OCPolarity_High;
-
-    TIM_OC1Init(TIM3, &outputChannelInit);
+    TIM_OC1Init(TIM3, &channel);
     TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
 
     GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_1);
 
+    global_period = period;
+
     return STATUS_SUCCESS;
 }
 
-void pwm_ch_freq(uint32_t pwm_freq, float duty) {
-    uint32_t clk_freq, period;
-    status_t st;
+#define MAX_DUTY 256
+status_t pwm_ch_duty(uint32_t duty) {
+    uint32_t period = global_period;
 
-    st = init_timer(pwm_freq, &clk_freq, &period);
-    if (FAIL(st)) {
-        return;
+    uint32_t new_duty = period * (((float)duty)/MAX_DUTY);
+    if (new_duty > CCR_MAX) {
+        return STATUS_ERROR;
     }
 
-    TIM_OCInitTypeDef outputChannelInit = {0,};
+    TIM_SetCompare1(TIM3, new_duty);
 
-    outputChannelInit.TIM_OCMode = TIM_OCMode_PWM1;
-
-    outputChannelInit.TIM_Pulse = period * duty;
-    outputChannelInit.TIM_OutputState = TIM_OutputState_Enable;
-    outputChannelInit.TIM_OCPolarity = TIM_OCPolarity_High;
-
-    TIM_OC1Init(TIM3, &outputChannelInit);
-    TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
+    return STATUS_SUCCESS;
 }
 
+void init_usart() {
+    USART_InitTypeDef USART_InitStructure;
+
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+    USART_Init(USART1, &USART_InitStructure);
+
+    /* Output a message on Hyperterminal using printf function */
+    printf("\n\rUSART Printf Example: retarget the C library printf function to the USART\n\r");
+
+    /* Loop until the end of transmission */
+    /* The software must wait until TC=1. The TC flag remains cleared during all data
+       transfers and it is set by hardware at the last frames end of transmission*/
+    while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET)
+    {}
+}
 
 
 int main(void)
 {
     status_t st;
+
+    //init_usart();
+
     RCC->AHBENR |= RCC_AHBENR_GPIOCEN;  // enable the clock to GPIOC
     GPIOC->MODER = (1 << 16);
+    SysTick_Config(SystemCoreClock/100);
 
-    st = init_pwm(733, .5);
+    st = init_pwm(1000);
     if (FAIL(st)){
         GPIOC->ODR = (1 << 8);
     }
 
-    //SysTick_Config(SystemCoreClock/100);
     while(1);
 }
