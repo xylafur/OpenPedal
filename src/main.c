4c5d14bb343b989eb5534fb3a454325367074508
xylafur <james@jkesley.com>
2020-06-28 16:12:39 -0500

    Generated 440Hz sin wave in main

--- src/main.c
+++ src/main.c
@@ -4,17 +4,27 @@
 #include "generate.h"
 #include "transform.h"
 
-uint32_t do_change = 0;
+static uint32_t do_change = 0;
+
+#define NUM_SAMPLES 128
+//#define NUM_SAMPLES 2
+
+uint32_t SIN_TABLE [NUM_SAMPLES] = {
+    128, 134, 140, 146, 152, 159, 165, 171, 176, 182, 188, 193, 199, 204, 209,
+    213, 218, 222, 226, 230, 234, 237, 240, 243, 246, 248, 250, 252, 253, 254,
+    255, 255, 256, 255, 255, 254, 253, 252, 250, 248, 246, 243, 240, 237, 234,
+    230, 226, 222, 218, 213, 209, 204, 199, 193, 188, 182, 176, 171, 165, 159,
+    152, 146, 140, 134, 128, 121, 115, 109, 103, 96, 90, 84, 79, 73, 67, 62,
+    56, 51, 46, 42, 37, 33, 29, 25, 21, 18, 15, 12, 9, 7, 5, 3, 2, 1, 0, 0, 0,
+    0, 0, 1, 2, 3, 5, 7, 9, 12, 15, 18, 21, 25, 29, 33, 37, 42, 46, 51, 56, 62,
+    67, 73, 79, 84, 90, 96, 103, 109, 115, 121
+};
+
+uint32_t SIN_MAX = 512;
 
 void SysTick_Handler(void) {
-    static uint32_t duty = 0;
-
-    pwm_ch_duty(duty);
-
-    if (duty != 255) {
-        duty = 255;
-    } else {
-        duty = 0;
+    if (!do_change) {
+        do_change = 1;
     }
 
     do_change = 1;
@@ -22,18 +32,21 @@ void SysTick_Handler(void) {
 
 #define GENERATE_FREQUENCY (SystemCoreClock/100)
 
-
 int main(void)
 {
     status_t st;
     uint32_t newval;
     uint32_t max = 0;
+    uint32_t time = 0;
+
+    uint32_t systick_clicks = SystemCoreClock / (NUM_SAMPLES * 440);
 
     //init_usart();
 
     RCC->AHBENR |= RCC_AHBENR_GPIOCEN;  // enable the clock to GPIOC
     GPIOC->MODER = (1 << 16);
-    SysTick_Config(54545);
+
+    SysTick_Config(systick_clicks);
 
     st = init_transform(GENERATE_FREQUENCY, TRANSFORM_CONSTANT);
     if (FAIL(st)){
@@ -47,23 +60,18 @@ int main(void)
         DEBUG();
     }
 
+    uint32_t ii = 0;
 
     while(1) {
-    //    if (do_change) {
-    //        do_change = 0;
+        // Constant polling of do_change causes execution to get stuck.. wait a
+        // few cycles before checking for update
+        if (!((ii++)%3)) {
+            continue;
+        }
 
-    //        /* If there is nothing new in the buffer.. just return from the handler */
-    //        if (FAIL(get_value(&newval))) {
-    //            continue;
-    //        }
-
-    //        /* Convert new value into a form that the generate module can use */
-    //        newval = amplitude_to_duty(newval, max);
-
-    //        /* Turn on the LED if anything goes wrong */
-    //        if (FAIL(pwm_ch_duty(newval))){
-    //          GPIOC->ODR = (1 << 8);
-    //        }
-    //    }
+        if (do_change) {
+            pwm_ch_duty(SIN_TABLE[(time++)%NUM_SAMPLES]);
+            do_change = 0;
+        }
     }
 }
